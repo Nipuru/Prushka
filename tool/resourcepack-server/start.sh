8e2f6b567b6efd90b867092d7a206f2e94184af2
Nipuru <2789129195@qq.com>
2025-08-13 22:01:35 +0800

    refactor: 删除资源包服务器目录

--- tool/resourcepack-server/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-python3 run.py > nohup.out 2>nohup.out &
-echo $! > run.pid
--- tool/resourcepack-server/stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-pid_file="run.pid"
-if [ -f "$pid_file" ]; then
-	pid=$(cat $pid_file)
-	kill -9 $pid
-	rm -f $pid_file
-fi
-
