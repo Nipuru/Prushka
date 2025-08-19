e964830ccce20f2dc80c97628c975e6259d59abc
Nipuru <2789129195@qq.com>
2025-08-19 16:55:52 +0800

    refactor(server-bukkit): 优化物品栏空格子数计算

--- server/server-bukkit/src/main/kotlin/server/bukkit/util/BukkitUtils.kt
+++ server/server-bukkit/src/main/kotlin/server/bukkit/util/BukkitUtils.kt
@@ -3,27 +3,22 @@ package server.bukkit.util
 import org.bukkit.Bukkit
 import org.bukkit.event.Listener
 import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.Plugin
 
 
 /**
- * @author Nipuru
- * @since 2024/11/12 18:11
+ * 注册监听器
  */
 fun Listener.register(plugin: Plugin) {
     Bukkit.getPluginManager().registerEvents(this, plugin)
 }
 
-fun Inventory.getEmptySlot(): Int {
-    var slot = 0
-    for (itemStack in storageContents) {
-        if (itemStack == null) {
-            ++slot
-        } else if (itemStack.type.isAir) {
-            ++slot
-        } else if (itemStack.amount == 0) {
-            ++slot
-        }
+/**
+ * 获取物品栏空格子数
+ */
+fun Inventory.emptySlot(): Int {
+    return storageContents.count { itemStack ->
+        itemStack == null || itemStack.isEmpty
     }
-    return slot
 }
