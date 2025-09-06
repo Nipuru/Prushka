0e5e27b1f4867d3a86689cdbda4694e36dbf943a
Nipuru <2789129195@qq.com>
2025-09-06 20:55:53 +0800

    refactor(auth): 优化玩家加载逻辑

--- server/server-auth/src/main/kotlin/server/auth/schema/PlayerTable.kt
+++ server/server-auth/src/main/kotlin/server/auth/schema/PlayerTable.kt
@@ -13,7 +13,7 @@ object PlayerTable : Table() {
     override val tableName = "tb_player"
     val playerId = integer("player_id").autoIncrement(Sequence("player_id_seq", startWith = 100000))
     val name = varchar("name", 16)
-    val uniqueId = varchar("unique_id", 36).uniqueIndex()
+    val uniqueId = uuid("unique_id").uniqueIndex()
     val lastIp = text("last_ip")
     val dbId = integer("db_id")
     val createTime = long("create_time")
--- server/server-auth/src/main/kotlin/server/auth/service/PlayerServiceImpl.kt
+++ server/server-auth/src/main/kotlin/server/auth/service/PlayerServiceImpl.kt
@@ -4,6 +4,7 @@ import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.update
 import server.auth.schema.PlayerTable
 import server.common.message.PlayerMessage
 import server.common.service.PlayerService
@@ -17,24 +18,28 @@ import java.util.*
 class PlayerServiceImpl: PlayerService {
     override fun load(name: String, uniqueId: UUID, lastIp: String): PlayerMessage {
         return transaction {
-            val rs = PlayerTable.selectAll().where(PlayerTable.uniqueId eq uniqueId.toString())
+            val rs = PlayerTable.selectAll().where(PlayerTable.uniqueId eq uniqueId)
                 .singleOrNull()
             if (rs != null) {
-                return@transaction PlayerMessage(
-                    rs[PlayerTable.playerId],
-                    rs[PlayerTable.dbId]
-                )
+                PlayerTable.update({ PlayerTable.uniqueId eq uniqueId }) {
+                    it[PlayerTable.lastIp] = lastIp
+                }
+                PlayerMessage(rs[PlayerTable.playerId], rs[PlayerTable.dbId])
+            } else {
+                val dbId = selectDbId()
+                val playerId = PlayerTable.insert {
+                    it[this.name] = name
+                    it[this.uniqueId] = uniqueId
+                    it[this.lastIp] = lastIp
+                    it[this.dbId] = dbId
+                    it[this.createTime] = System.currentTimeMillis()
+                } get PlayerTable.playerId
+                PlayerMessage(playerId, dbId)
             }
-
-            val dbId = 1
-            val playerId = PlayerTable.insert {
-                it[this.name] = name
-                it[this.uniqueId] = uniqueId.toString()
-                it[this.lastIp] = lastIp
-                it[this.dbId] = dbId
-                it[this.createTime] = System.currentTimeMillis()
-            } get PlayerTable.playerId
-            return@transaction PlayerMessage(playerId, dbId)
         }
+    }
+
+    private fun selectDbId(): Int {
+        return 1 // todo
     }
 }
