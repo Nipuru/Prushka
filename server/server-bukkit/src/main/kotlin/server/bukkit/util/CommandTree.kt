fef5b65b931227200a06c076b5f21bd2a19cee80
Nipuru <2789129195@qq.com>
2025-11-02 14:16:22 +0800

    fix(brigadier): 复制节点以解决重定向不生效的问题

--- server/server-bukkit/src/main/kotlin/server/bukkit/util/CommandTree.kt
+++ server/server-bukkit/src/main/kotlin/server/bukkit/util/CommandTree.kt
@@ -2,6 +2,7 @@ package server.bukkit.util
 
 import com.mojang.brigadier.tree.LiteralCommandNode
 import io.papermc.paper.command.brigadier.CommandSourceStack
+import io.papermc.paper.command.brigadier.Commands
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents
 import org.bukkit.plugin.Plugin
 
@@ -18,7 +19,23 @@ interface CommandTree {
 
     fun register(plugin: Plugin) {
         plugin.lifecycleManager.registerEventHandler(LifecycleEvents.COMMANDS) { commands ->
-            commands.registrar().register(root, description, aliases)
+            commands.registrar().register(root, description)
+            aliases.map { alias ->
+                buildRedirect(alias, root)
+            }.forEach { commands.registrar().register(it, description) }
         }
+    }
+
+    // 复制节点以解决重定向不生效的问题
+    // https://github.com/Mojang/brigadier/issues/46
+    fun buildRedirect(alias: String, destination: LiteralCommandNode<CommandSourceStack>): LiteralCommandNode<CommandSourceStack> {
+        val builder = Commands.literal(alias.lowercase())
+            .requires(destination.getRequirement())
+            .forward(destination.getRedirect(), destination.getRedirectModifier(), destination.isFork)
+            .executes(destination.getCommand())
+        for (child in destination.getChildren()) {
+            builder.then(child)
+        }
+        return builder.build()
     }
 }
