211dd2fe06a677e7567c6ac9ebbeaf9f4eee966d
Nipuru <2789129195@qq.com>
2025-12-24 20:31:05 +0800

    chore(database): 优化数据库列注册逻辑

--- server/server-common/src/main/kotlin/server/common/util/database/Extensions.kt
+++ server/server-common/src/main/kotlin/server/common/util/database/Extensions.kt
@@ -1,8 +1,27 @@
 package server.common.util.database
 
+import org.jetbrains.exposed.sql.ArrayColumnType
+import org.jetbrains.exposed.sql.BasicBinaryColumnType
+import org.jetbrains.exposed.sql.BooleanColumnType
+import org.jetbrains.exposed.sql.ByteColumnType
+import org.jetbrains.exposed.sql.CharacterColumnType
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.ColumnType
+import org.jetbrains.exposed.sql.DecimalColumnType
+import org.jetbrains.exposed.sql.DoubleColumnType
+import org.jetbrains.exposed.sql.FloatColumnType
+import org.jetbrains.exposed.sql.IntegerColumnType
+import org.jetbrains.exposed.sql.LongColumnType
 import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.ShortColumnType
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.TextColumnType
+import org.jetbrains.exposed.sql.UUIDColumnType
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.math.BigDecimal
+import java.math.MathContext
+import java.util.UUID
+import kotlin.reflect.KClass
 
 /**
  * @author Nipuru
@@ -12,5 +31,30 @@ fun Table.initSchema() {
     transaction {
         SchemaUtils.create(this@initSchema)
         SchemaUtils.createMissingTablesAndColumns(this@initSchema)
+    }
+}
+
+@Suppress("UNCHECKED_CAST")
+fun Table.registerColumn(name: String, clazz: KClass<*>, default: Any): Column<*> {
+    val columnType = when (clazz) {
+        Boolean::class -> BooleanColumnType()
+        Byte::class -> ByteColumnType()
+        Short::class -> ShortColumnType()
+        Int::class -> IntegerColumnType()
+        Long::class -> LongColumnType()
+        Float::class -> FloatColumnType()
+        Double::class -> DoubleColumnType()
+        String::class -> TextColumnType()
+        Char::class -> CharacterColumnType()
+        ByteArray::class -> BasicBinaryColumnType()
+        BigDecimal::class -> DecimalColumnType(MathContext.DECIMAL64.precision, 20)
+        UUID::class -> UUIDColumnType()
+        else -> error("Unsupported column type: $clazz")
+    } as ColumnType<Any>
+    val fieldName = name.replace("([a-z])([A-Z])".toRegex(), "$1_$2").lowercase()
+    return if (default is List<*>) {
+        registerColumn(fieldName, ArrayColumnType(columnType)).default(default)
+    } else {
+        registerColumn(fieldName, columnType).default(default)
     }
 }
--- server/server-database/src/main/kotlin/server/database/schema/PlayerDataTable.kt
+++ server/server-database/src/main/kotlin/server/database/schema/PlayerDataTable.kt
@@ -1,12 +1,11 @@
 package server.database.schema
 
-import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.statements.UpdateBuilder
 import server.common.message.PlayerDataMessage.TableInfo
-import java.math.BigDecimal
-import java.math.MathContext
-import java.util.*
-import kotlin.reflect.KClass
+import server.common.util.database.registerColumn
 
 
 /**
@@ -16,58 +15,33 @@ import kotlin.reflect.KClass
 class PlayerDataTable(tableInfo: TableInfo) : Table() {
     override val tableName = tableInfo.tableName
     val playerId = integer("player_id")
-    val columnMap = mutableMapOf<String, Pair<KClass<*>, Column<*>>>()
+    val columnMap = mutableMapOf<String, Column<*>>()
     override val primaryKey: PrimaryKey
 
     init {
         for ((name, clazz, default) in tableInfo.fields) {
             val kClass = clazz.kotlin
             val column = registerColumn(name, kClass, default)
-            columnMap[name] = kClass to column
+            columnMap[name] = column
         }
-        val uniqueColumn = tableInfo.uniqueKeys.map { columnMap[it]!!.second }.toTypedArray()
+        val uniqueColumn = tableInfo.uniqueKeys.map { column(it) }.toTypedArray()
         primaryKey = PrimaryKey(playerId, *uniqueColumn)
     }
 
     @Suppress("UNCHECKED_CAST")
     fun column(name: String): Column<Any> {
-        val (_, column) = columnMap[name]!!
+        val column = columnMap[name]!!
         return column as Column<Any>
     }
 
     @Suppress("UNCHECKED_CAST")
     fun setColumn(statement: UpdateBuilder<*>, name: String, value: Any) {
-        val (_, column) = columnMap[name]!!
+        val column = columnMap[name]!!
         statement[column as Column<Any>] = value
     }
 
     fun getColumn(row: ResultRow, name: String): Any {
-        val (_, column) = columnMap[name]!!
+        val column = columnMap[name]!!
         return row[column]!!
-    }
-
-    @Suppress("UNCHECKED_CAST")
-    private fun registerColumn(name: String, clazz: KClass<*>, default: Any): Column<*> {
-        val columnType = when (clazz) {
-            Boolean::class -> BooleanColumnType()
-            Byte::class -> ByteColumnType()
-            Short::class -> ShortColumnType()
-            Int::class -> IntegerColumnType()
-            Long::class -> LongColumnType()
-            Float::class -> FloatColumnType()
-            Double::class -> DoubleColumnType()
-            String::class -> TextColumnType()
-            Char::class -> CharacterColumnType()
-            ByteArray::class -> BasicBinaryColumnType()
-            BigDecimal::class -> DecimalColumnType(MathContext.DECIMAL64.precision, 20)
-            UUID::class -> UUIDColumnType()
-            else -> error("Unsupported column type: $clazz")
-        } as ColumnType<Any>
-        val fieldName = name.replace("([a-z])([A-Z])".toRegex(), "$1_$2").lowercase()
-        return if (default is List<*>) {
-            registerColumn(fieldName, ArrayColumnType(columnType)).default(default)
-        } else {
-            registerColumn(fieldName, columnType).default(default)
-        }
     }
 }
